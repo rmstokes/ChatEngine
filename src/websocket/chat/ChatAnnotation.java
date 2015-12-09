353ba35498dd77bd844f40012f81d4b2385f4856
rmstokes <rmstoke1@aggies.ncat.edu>
2015-12-08 19:16:01 -0500

    User Names are assigned color for aesthetics inside chat room

--- src/websocket/chat/ChatAnnotation.java
+++ src/websocket/chat/ChatAnnotation.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.AbstractSet;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Set;
@@ -58,45 +59,56 @@ import util.HTMLFilter;
 public class ChatAnnotation {
 
 	private static final Log log = LogFactory.getLog(ChatAnnotation.class);
-	
+
 	private static final String GUEST_PREFIX = "Guest";
 	private static final AtomicInteger connectionIds = new AtomicInteger(0);
 	private static final Set<ChatAnnotation> connections = new CopyOnWriteArraySet<>();
-		
-	private static String nickname;
+
+	private String nickname;
 	private Session session;
-	//private static String adminID = null;
+	// private static String adminID = null;
 	private static boolean adminCreatedGroups = false;
 	private static Hashtable<Integer, HashSet<String>> groupTable = new Hashtable<Integer, HashSet<String>>();
 
+	// create color vector to assign colors to members of each chat group
+	// format: HashTable<Group#, Hashtable<color, count>> ex. Group 1: red->1;
+	// Group1: blue->0
+	private static Hashtable<Integer, Hashtable<String, Integer>> colorVector = new Hashtable<Integer, Hashtable<String, Integer>>();
+
+	// create clientColor variable to store username color for each client
+	private static Hashtable<String, String> clientColor = new Hashtable<String, String>();
+
 	public ChatAnnotation() {
-		//a new ChatAnnotation object is created for every Client page that connects to server.
+		// a new ChatAnnotation object is created for every Client page that
+		// connects to server.
 		System.out.println("chat annotation constructor");
 	}
 
 	@OnOpen
 	public void start(Session session, @PathParam("path") String path) {
-		//onopen called when client side websocket initiates connection to this server.
-		//path argument identifies which client page is on other end
-		//for 'landing' page connections add the ChatAnnotation object to static list.
+		// onopen called when client side websocket initiates connection to this
+		// server.
+		// path argument identifies which client page is on other end
+		// for 'landing' page connections add the ChatAnnotation object to
+		// static list.
 		System.out.println("inside start OnOpen");
 		System.out.println("path: " + path);
 		this.session = session;
 		System.out.println("real session id: " + session.getId());
-		
+
 		switch (path) {
 		case "admin":
-			System.out.println("admin connected.");			
+			System.out.println("admin connected.");
 			return;
 		case "login":
 			// if admin has not created groups then don't allow client to
 			// connect.
 			System.out.println("login page connected.");
-			return;		
+			return;
 		case "chat":
 			// if admin has not created groups then don't allow client to
 			// connect.
-			System.out.println("chat page connected.");			
+			System.out.println("chat page connected.");
 			System.out.println("adminCreatedGroups: " + adminCreatedGroups);
 			return;
 		case "landing":
@@ -108,23 +120,44 @@ public class ChatAnnotation {
 			return;
 
 		}
-		
+
 	}
 
 	@OnClose
-	public void end() {
+	public void end(Session session, @PathParam("path") String path) {
+		System.out.println("inside end method...");
 		connections.remove(this);
-		// remove user from appropriate group
-
-		// need to change this to xml format
-		String message = String.format("* %s %s", nickname, "has disconnected...");
-
-		try {
-			broadcast(message);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		if (!path.equals("chat")) return; 		
+		
+		//decrement colorVector value 
+		//format: HashTable<Group#, Hashtable<color, count>> ex. Group 1: red->1;
+		String senderID = session.getId();
+		int originalGroupNumber = getSenderGroup(senderID);
+		String username = this.nickname;
+		System.out.println("username: "+username);
+		String color = clientColor.get(username);
+		System.out.println("color: "+color);
+		Hashtable<String, Integer> colorCount = colorVector.get(originalGroupNumber);
+		int count = colorCount.get(color);
+		count--;
+		colorCount.put(color, count);
+		colorVector.put(originalGroupNumber, colorCount);
+		
+		//remove username from clientColor
+		//format: Hashtable<nickname, color> pair in clientColor variable
+		clientColor.remove(username);
+		
+		
+		// send message to chat group users		
+		String message = "<message type='alert' senderID='" + session.getId() + "' groupNumber='" + originalGroupNumber
+				+ "'><text>" + username + " has left group.</text></message>";
+		
+		  try { 
+			  broadcast(message); 
+		  } catch (Exception e) { 
+			  e.printStackTrace(); 
+		  }
+		 
 	}
 
 	@OnMessage
@@ -140,16 +173,15 @@ public class ChatAnnotation {
 		InputSource is = new InputSource(sr);
 		Document doc = builder.parse(is);
 
-		// get message type 
+		// get message type
 		Element element = doc.getDocumentElement();
 		String messageType = element.getAttribute("type");
-		
+
 		String senderID = null;
 		if (messageType.equals("typing") || messageType.equals("chat")) {
-			// add origin senderID attribute to the document before broadcasting
-			// it.						
-			senderID = element.getAttribute("senderID");			
-			// now that the senderID has been added to document
+			
+			senderID = element.getAttribute("senderID");
+			
 			// transform the updated document into a string before broadcasting
 			DOMSource domSource = new DOMSource(doc);
 			StringWriter writer = new StringWriter();
@@ -159,6 +191,10 @@ public class ChatAnnotation {
 			transformer.transform(domSource, result);
 			message = writer.toString();
 			System.out.println("broadcasting message");
+			
+			// assign senderColor attribute before broadcasting
+			String senderColor = getSenderColor(senderID);
+			element.setAttribute("senderColor", senderColor);
 			broadcast(message);
 		} else if (messageType.equals("groupCreation")) {
 			// this is message from admin page to create groups
@@ -176,13 +212,27 @@ public class ChatAnnotation {
 			adminCreatedGroups = true;
 			System.out.println("groups created");
 
+			// initialize colorCount variable with the color names and the count
+			// key value: <color, count>
+			int count = 0;
+			Hashtable<String, Integer> colorCount = new Hashtable<String, Integer>();
+			colorCount.put("red", count);
+			colorCount.put("green", count);
+			colorCount.put("blue", count);
+			colorCount.put("orange", count);
+
+			// initialize group numbers for colorVector
+			for (int key = 1; key <= numGroups; key++) {
+				// make each group# have it's own colors to choose from
+				colorVector.put(key, colorCount);
+			}
+
 			// send message to client to invoke a new web page that displays
 			// group creation confirmation and link to login page possibly
 			senderID = element.getAttribute("senderID");
 			String msg = "<message type='groupsCreated' senderID='" + senderID + "'></message>";
 			session.getBasicRemote().sendText(msg);
-			
-			
+
 		} else if (messageType.equals("setUserType")) {
 			// this is a message from landing page drop down menu
 			// session contains the initial client id.
@@ -213,7 +263,8 @@ public class ChatAnnotation {
 			}
 		} else if (messageType.equals("joinGroup")) {
 			// add user to proper group
-			String nickname = element.getAttribute("username");
+			//String nickname = element.getAttribute("username");
+			nickname = element.getAttribute("username");
 			// nickname = username;
 			senderID = element.getAttribute("senderID");
 			System.out.println("joinGroup message received.");
@@ -228,41 +279,68 @@ public class ChatAnnotation {
 			String msg3 = "<message type='displayChat' senderID='" + senderID + "'></message>";
 			session.getBasicRemote().sendText(msg3);
 			System.out.println("addedToList: " + addedToList);
-			
 
 		} else if (messageType.equals("requestID")) {
 			senderID = session.getId();
 			String msg4 = "<message type='alert' senderID='" + senderID + "'></message>";
 			session.getBasicRemote().sendText(msg4);
 		} else if (messageType.equals("broadcast")) {
+			// broadcast message means user has just joined chat group.
 			// map the current session's sender id to the connection id
 			// established on initial
 			// connection from landing page
 
 			// send message to group indicating who has just joined.
 			System.out.println("broadcast message type detected.");
-			String nickname = element.getAttribute("username");
+			//String nickname = element.getAttribute("username");
+			nickname = element.getAttribute("username");
 			senderID = element.getAttribute("senderID"); // original senderID
-			System.out.println("senderID: "+senderID);
-			// String msg5 = "<message type='alert'
-			// senderID='"+senderID+"'><text>"+nickname+" has joined
-			// group.</text></message>";
+			System.out.println("senderID: " + senderID);
 
 			// establish link between current session.id() and original senderID
 			// these values will be different so they need to be mapped to one
 			// another
 			// so that we can respond to the proper users within a group
-			
+
 			// place new senderID into group and remove old senderID from group
 			Integer originalGroupNumber = getSenderGroup(senderID);
+			System.out.println("original group number: " + originalGroupNumber);
 			HashSet<String> group = groupTable.get(originalGroupNumber);
 			group.remove(senderID);
 			group.add(session.getId());
 
-			// send message to group that new user has joined session.
+			// for each <group, client> pair assign a color from colorVector
+			// (based on the minimum count in the colorVector)			
+			Hashtable<String, Integer> colorAndValueTable = colorVector.get(originalGroupNumber);
+			// loop through colorAndValueTable to find color that has minimum
+			// value.
+			Set<String> keys = colorAndValueTable.keySet();
+			System.out.println("colorVector keyset size: " + keys.size());
+			int min = 999;// sentinel value
+			String senderColor = "";
+			for (String color : keys) {
+				int colorCount = colorAndValueTable.get(color);
+				if (colorCount < min) {
+					min = colorCount;
+					senderColor = color;
+				}
+			}
+			// increment color variable in colorVector<Group#,
+			// Hashtable<senderColor, count>>
+			int count = colorVector.get(originalGroupNumber).get(senderColor);
+			count++;
+			colorVector.get(originalGroupNumber).put(senderColor, count);
+			System.out.println("sender color: " + senderColor);
+			
+			//store the <nickname, color> pair in clientColor variable
+			clientColor.put(nickname, senderColor);
+
 			String updatedID = session.getId();
+
+			// send message to group that new user has joined session.
 			String msg5 = "<message type='alert' senderID='" + senderID + "' updatedID='" + updatedID
-					+ "' groupNumber='" + originalGroupNumber + "'><text>"+nickname+" has joined group.</text></message>";
+					+ "' groupNumber='" + originalGroupNumber + "' senderColor='" + senderColor + "'><text>" + nickname
+					+ " has joined group.</text></message>";
 			System.out.println("calling broadcast with alert message.");
 			broadcast(msg5);
 		}
@@ -275,14 +353,20 @@ public class ChatAnnotation {
 
 	private void broadcast(String msg) throws Exception {
 		// broadcast message to members of same group.
-
+		System.out.println("inside broadcast...");
+		System.out.println("broadcast msg: " + msg);
 		// access xml Document
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		System.out.println("after factory...");
 		DocumentBuilder builder;
 		builder = factory.newDocumentBuilder();
+		System.out.println("after builder...");
 		StringReader sr = new StringReader(msg);
+		System.out.println("after sr...");
 		InputSource is = new InputSource(sr);
+		System.out.println("after is...");
 		Document doc = builder.parse(is);
+		System.out.println("after doc...");
 
 		System.out.println("**adminCreatedGroups: " + adminCreatedGroups);
 
@@ -297,13 +381,13 @@ public class ChatAnnotation {
 
 		System.out.println("senderID: " + senderID);
 		Integer senderGroup = getSenderGroup(senderID);
-		
+
 		// if the message is an initial connection from chat client
 		// it will have an attribute called updatedID
 		// in those cases, update the connections list with new info
 		if (element.getAttribute("updatedID") != "") {
 			System.out.println("initial connection from chat page detected");
-			//set senderGroup based on xml attribute
+			// set senderGroup based on xml attribute
 			senderGroup = Integer.parseInt(element.getAttribute("groupNumber"));
 			// update connection list with new client identification
 			connections.add(this);
@@ -316,9 +400,16 @@ public class ChatAnnotation {
 
 		}
 		System.out.println("senderGroup: " + senderGroup);
-		System.out.println("updatedID: "+element.getAttribute("updatedID"));
+		System.out.println("updatedID: " + element.getAttribute("updatedID"));
 		// set variable to store clientIDs for senderGroup
 		HashSet<String> broadcastGroupMembers = groupTable.get(senderGroup);
+
+		// set senderColor attribute in message
+		/*
+		 * String sendername = element.getAttribute("sendername"); String color
+		 * = clientColor.get(sendername); element.setAttribute("senderColor",
+		 * color);
+		 */
 
 		// loop through all clients in connections list and send message to them
 		// if their id is in broadcastGroup
@@ -378,10 +469,28 @@ public class ChatAnnotation {
 					// sender group number located
 					System.out.println("match in group number: " + groupNumber);
 					senderGroup = groupNumber;
-					break;
+					return senderGroup;
 				}
 			}
 		}
 		return senderGroup;
+	}
+
+	private String getSenderColor(String senderID) {
+		Integer originalGroupNumber = getSenderGroup(senderID);
+		Hashtable<String, Integer> colorAndValueTable = colorVector.get(originalGroupNumber);
+		// loop through colorAndValueTable to find color that has minimum value.
+		Set<String> keys = colorAndValueTable.keySet();		
+		int min = 999;// sentinel value
+		String senderColor = "";
+		for (String color : keys) {
+			int colorCount = colorAndValueTable.get(color);
+			if (colorCount < min) {
+				min = colorCount;
+				senderColor = color;
+			}
+		}
+		System.out.println("senderColor assigned: "+senderColor);
+		return senderColor;
 	}
 }
